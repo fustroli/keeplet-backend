08b1f6d0fecd4087c07e8883180b4012386cc488
Roland Füst <fustroli@gmail.com>
2025-05-31 08:26:23 +0200

    chore: app config setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,35 @@
-import { NestFactory } from '@nestjs/core';
+import * as cookieParser from 'cookie-parser';
+
 import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { appConfig } from 'src/config';
+import helmet from 'helmet';
+import { json } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const port = appConfig.PORT;
+
+  //secure app by setting HTTP response headers
+  app.use(helmet());
+
+  app.use(cookieParser());
+
+  //enable cors
+  app.enableCors({ origin: appConfig.FRONTEND_URL, credentials: true });
+
+  //validation pipeline
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  app.use(json({ limit: '20kb' }));
+
+  await app.listen(port);
 }
 bootstrap();
